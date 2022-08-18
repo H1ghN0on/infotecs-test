235d04a7ecd419bcad86c29481aaa02e4cb662e2
Igor <devNoon@yandex.ru>
2022-08-18 18:08:16 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="logo">ToDo</div>
       </header>
+
+      <aside>
+        <div className="aside__item search">
+          <input placeholder="Поиск..." type="text" className="search__input" />
+        </div>
+        <div className="aside__item add">+ Добавить</div>
+        <div className="aside__item note active done">Dashboard</div>
+        <div className="aside__item note pending">Dashboard</div>
+        <div className="aside__item note waiting">Dashboard</div>
+        <div className="aside__item note done">Dashboard</div>
+      </aside>
+
+      <main className="page-wrapper">
+        <div className="note">
+          <div className="note__avatar">
+            <img src="/chiaki.jpg" alt="" />
+          </div>
+          <div className="note__name">Win the shogun raiden battle</div>
+          <div className="note__text">
+            I will crush Ei and her doll. I will abolish the vision hunt decree
+          </div>
+          <div className="note__status-list">
+            <div className="note__status-btn waiting">Ожидает</div>
+            <div className="note__status-btn pending">В процессе</div>
+            <div className="note__status-btn active done">Выполнено</div>
+          </div>
+          <div className="note__danger-zone">
+            <div className="note__danger-btn remove">Удалить</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
